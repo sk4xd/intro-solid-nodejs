929a87ddcfc7bf133ca87029cba27c5a3ec369c6
Jorge Augusto Delgado <jorge.delgado@hotmail.com.br>
2021-08-17 14:32:08 -0300

    feat: Swagger doc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 import { usersRoutes } from "./routes/users.routes";
 
@@ -7,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
